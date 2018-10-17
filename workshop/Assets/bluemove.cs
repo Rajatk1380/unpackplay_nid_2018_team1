174fab3b8ba41b7641b9d46e2fd48e06b1decb36
abhikun <abhipatiri@gmail.com>
2018-10-17 14:33:18 +0530

    Update workshop/Assets/bluemove.cs
    script for moving blue paddle

---
+++ workshop/Assets/bluemove.cs
@@ -0,0 +1,30 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bluemove : MonoBehaviour {
+
+	// Use this for initialization
+
+		public float moveSpeed = 5f;
+		private Rigidbody2D rb2d;
+	
+	
+	// Update is called once per frame
+	//void Start()
+	//{
+	//	rb2d=
+	//}
+	void Update () {
+		if (Input.GetKey (KeyCode.W)) {
+			transform.Translate (-Vector2.left* moveSpeed * Time.deltaTime);
+		} else if (Input.GetKey (KeyCode.S)) {
+			transform.Translate (Vector2.left * moveSpeed * Time.deltaTime);
+
+		} else 
+		{
+			
+		}
+	}
+}
